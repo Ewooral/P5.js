b709a1e0be440913dea4b9e85c1ee8c08ba4ef88
Ewooral <boahen.cos@gmail.com>
2022-03-20 23:50:05 +0000

    P5 JS server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 let path = require('path');
-let fs = require('fs');
+// let fs = require('fs');
 
 // fs.readFile('./HTML/index.html', 'utf8', (err, data) => {
 //   if (err) throw err;
@@ -7,19 +7,19 @@ let fs = require('fs');
 
 // })
 
-let handleRequest = function (request, response) {
-  response.writeHead(200, {'Content-Type': 'html'});
-  response.end(`
-    <h1>Hello, world!</h1>
-  `)
+// let handleRequest = function (request, response) {
+//   response.writeHead(200, {'Content-Type': 'html'});
+//   response.end(`
+//     <h1>Hello, world!</h1>
+//   `)
     
-};
+// };
 
 
-const http = require('http');
-let server = http.createServer(handleRequest);
-server.listen(8080, pasteAMessage());
+// const http = require('http');
+// let server = http.createServer(handleRequest);
+// server.listen(8080, pasteAMessage());
 
-function pasteAMessage(){
-  console.log("Connected at port 8080")
-};
+// function pasteAMessage(){
+//   console.log("Connected at port 8080")
+// };
