7e47a98249606fffc6c4f8a4d63832531e4b5e78
Ewooral <boahen.cos@gmail.com>
2022-03-20 15:13:26 +0000

    creating a node server for the p5 js project

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+let path = require('path');
+let fs = require('fs');
+
+// fs.readFile('./HTML/index.html', 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+
+// })
+
 let handleRequest = function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+  response.writeHead(200, {'Content-Type': 'html'});
+  response.end(`
+    <h1>Hello, world!</h1>
+  `)
+    
 };
+
 
 const http = require('http');
 let server = http.createServer(handleRequest);
-server.listen(8080);
+server.listen(8080, pasteAMessage());
+
+function pasteAMessage(){
+  console.log("Connected at port 8080")
+};
