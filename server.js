9e62dd6c55b3073872ceafc3d39d169acd635677
Ewooral <boahen.cos@gmail.com>
2022-03-20 13:54:24 +0000

    P5 JS tutorials

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-let handleRequest = function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-};
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const http = require('http');
-let server = http.createServer(handleRequest);
-server.listen(8080);
+
+// EXPRESS JS 
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = express.Router();
+app.use('/', router);
+
+router.use(express.static("./public/"));
+
+router.get('/home',function(req,res){
+  res.sendFile(path.join(__dirname +'./public/HTML/index.html'));
+  //__dirname : It will resolve to your project folder.
+})
+
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
