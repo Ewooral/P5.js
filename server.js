d31e5834fedecbfc9d2c30be8081601ed25ac881
Ewooral <boahen.cos@gmail.com>
2022-03-20 23:52:19 +0000

    P5 JS server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ let handleRequest = function (request, response) {
 
 
 const http = require('http');
-// let server = http.createServer(handleRequest);
+let server = http.createServer(handleRequest);
 // server.listen(8080, pasteAMessage());
 
 // function pasteAMessage(){
