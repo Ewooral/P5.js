2d24910c9a6e7481d4ef7c78cf9d32e50da9bf76
Ewooral <boahen.cos@gmail.com>
2022-03-20 23:59:29 +0000

    P5 node server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ fs.readFile('./HTML/index.html', 'utf8', (err, data) => {
 // let handleRequest = function (request, response) {
 //   response.writeHead(200, {'Content-Type': 'html'});
 //   response.end(`
-//     <h1>Hello, world!</h1>
+    <h1>Hello, world!</h1>
   `)
     
 };
