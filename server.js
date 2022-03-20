9005eaeab3ec8e5630d8f8572ed3e87583850f7d
Ewooral <boahen.cos@gmail.com>
2022-03-20 23:50:57 +0000

    P5 JS server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ fs.readFile('./HTML/index.html', 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 
-// })
+})
 
 // let handleRequest = function (request, response) {
 //   response.writeHead(200, {'Content-Type': 'html'});
