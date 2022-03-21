8e822010a0af9ce0db1a47741dbdbcd3d4f914eb
Ewooral <boahen.cos@gmail.com>
2022-03-21 17:09:38 +0000

    creating P5.JS server using node JS

--- server.js
+++ server.js
@@ -1,25 +1,53 @@
-let path = require('path');
-let fs = require('fs');
-
-fs.readFile('./HTML/index.html', 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-
-})
+// let path = require('path');
+const fs = require('fs');
+const url = require('url');
+const http = require('http');
+const lookup = require("mime-types").lookup;
 
 let handleRequest = function (request, response) {
-  response.writeHead(200, {'Content-Type': 'html'});
-  response.end(`
-    <h1>Hello, world!</h1>
-  `)
+  let parsedURL = url.parse(request.url, true);
+
+  let path = parsedURL.path.replace(/^\/+|\/+$/g, '');
+
+  if(path == ""){
+    path = "index.html";
+  }
+  console.log(`Requested path ${path} `);
+
+  let file = __dirname + '/public/' + path;
+
+  fs.readFile(file, (err, data) => {
+   if (err) {
+      console.log(`File Not Found ${file}`);
+      response.writeHead(404);
+      response.end();
+    } else {
+      //specify the content type in the response
+      console.log(`Returning ${path}`);
+      response.setHeader("X-Content-Type-Options", "nosniff");
+      let mime = lookup(path);
+      response.writeHead(200, { "Content-type": mime });
+      // switch (path) {
+      //   case "main.css":
+      //     res.writeHead(200, { "Content-type": "text/css" });
+      //     break;
+      //   case "main.js":
+      //     res.writeHead(200, { "Content-type": "application/javascript" });
+      //     break;
+      //   case "index.html":
+      //     res.writeHead(200, { "Content-type": "text/html" });
+      // }
+      response.end(data);
+    }
+
+})
     
 };
 
 
-const http = require('http');
 let server = http.createServer(handleRequest);
 server.listen(8080, pasteAMessage());
 
 function pasteAMessage(){
-  console.log("Connected at port 8080")
+  console.log("Connected at port 8080");
 };
