12b80296f953877b0f068a90c0e0d0d4297b43c1
Ewooral <boahen.cos@gmail.com>
2022-03-25 00:57:50 +0000

    creating an arc in p5

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,43 +1,19 @@
-const { text } = require("express");
-
-let num = 60;
-let mx = [];
-let my = [];
-
-function setup(num = 60) {
-  createCanvas(2156, 2156);
+function setup(){
+  let cv = createCanvas(200,200);
+ 
   noStroke();
-  fill(255, 153);
-  for (let i = 0; i < num; i++) {
-    mx.push(i);
-    my.push(i);
-  }
-  
+  fill(102);
+
+  cv.position(50, 100);
+ 
 }
+
 
 function draw() {
-  background(237, 34, 93);
-
-  // Cycle through the array, using a different entry on each frame.
-  // Using modulo (%) like this is faster than moving all the values over.
-  let which = frameCount % num;
-  mx[which] = mouseX;
-  my[which] = mouseY;
-
-  for (let i = 0; i < num; i++) {
-    // which+1 is the smallest (the oldest in the array)
-    let index = (which + 1 + i) % num;
-    ellipse(mx[index], my[index], i, i);
-  }
-
-
-let hour = hours();
-let minutes = minutes();
-let seconds = seconds();
-
-fill(255);
-noStroke();
-text(hour + ' ' + minutes + ' ' + seconds, 10, 200)
-
-
+   background("red");
+  rect(a++ % width, 10, 2, 80);
+  
 }
+/**
+ * arc(x, y, w, h, start, stop, [mode], [detail])
+ */
